83691e7b86967f99e61c564b04858d7f1e6f642a
dev-marieclaire <mail.maryjamesb@gmail.com>
2026-09-20 17:39:58 -0600

    Bandera mexicana

---
+++ app/src/main/java/com/example/banderas/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.example.banderas
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.banderas.ui.theme.BanderasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BanderasTheme {
+                Scaffold(modifier = Modifier.fillMaxSize())
+                { innerPadding -> BanderaMexico(innerPadding) }
+            }
+        }
+    }
+}
+
+@Composable
+fun BanderaMexico(innerPadding: PaddingValues = PaddingValues.Zero, modifier: Modifier = Modifier)
+{
+    Row(modifier = Modifier.padding(innerPadding).fillMaxSize())
+    {
+        Box(modifier = Modifier
+            .weight(1f)
+            .fillMaxHeight()
+            .background(Color(0xFF006341)))
+        Box(modifier = Modifier
+            .weight(1f)
+            .fillMaxHeight()
+            .background(Color.White)
+            , contentAlignment = Alignment.Center)
+        {
+            Image(
+                painter = painterResource(id = R.drawable.escudo),
+                contentDescription = null
+            )
+        }
+        Box(modifier = Modifier.fillMaxHeight().weight(1f).background(Color.Red)) {}
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BanderasTheme {
+        BanderaMexico()
+    }
+}
